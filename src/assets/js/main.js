1def9a2b388232e0245bb8d5d7e249f2cc503642
ambientmoxie <contact@maximebenoit.work>
2024-10-03 09:26:39 +0200

    remove: dyn height css property function

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -9,14 +9,3 @@ function init() {
 }
 
 init();
-
-const documentHeight = () => {
-  const doc = document.documentElement;
-  doc.style.setProperty("--doc-height", `${window.innerHeight}px`);
-};
-
-window.addEventListener("resize", () => {
-  documentHeight();
-});
-
-documentHeight();
